d1db6fc319a3235de6c78f89820a792e735f92cb
ofcoder <ofcoder@gmail.com>
2023-08-21 04:18:37 +0300

    add components authorize

--- test.php
+++ test.php
@@ -2,12 +2,22 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Тестовая");
 ?><?$APPLICATION->IncludeComponent(
-	"bitrix:main.include",
+	"bitrix:main.auth.form",
 	"",
 	Array(
-		"AREA_FILE_SHOW" => "file",
-		"AREA_FILE_SUFFIX" => "inc",
-		"EDIT_TEMPLATE" => "",
-		"PATH" => "/include/phone.php"
+		"AUTH_FORGOT_PASSWORD_URL" => "",
+		"AUTH_REGISTER_URL" => "",
+		"AUTH_SUCCESS_URL" => ""
+	)
+);?>
+<?$APPLICATION->IncludeComponent(
+	"bitrix:system.auth.form",
+	"auth",
+	Array(
+		"COMPONENT_TEMPLATE" => ".default",
+		"FORGOT_PASSWORD_URL" => "/user/",
+		"PROFILE_URL" => "/user/profile.php",
+		"REGISTER_URL" => "/user/registration.php",
+		"SHOW_ERRORS" => "N"
 	)
 );?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
---
+++ user/.section.php
@@ -0,0 +1,6 @@
+<?
+$sSectionName = "пользователь";
+$arDirProperties = Array(
+
+);
+?>
